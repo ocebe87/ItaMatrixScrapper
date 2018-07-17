e17a07347a227cc1ac29de564609b46f6ecc9920
ser10 <sergio.ramiro@sky.uk>
2018-07-17 17:48:45 +0100

    Added extract for each flight offer

--- sample/pruebaSelenium.py
+++ sample/pruebaSelenium.py
@@ -1,13 +1,16 @@
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 BOX_ORIG_ = '//*[@id="cityPair-orig-0"]'
 BOX_DEST_ = '//*[@id="cityPair-dest-0"]'
 OUT_DATE_ = '//*[@id="cityPair-outDate-0"]'
 RET_DATE_ = '//*[@id="cityPair-retDate-0"]'
+SEARCH_BUTTON_ = '//*[@id="searchButton-0"]'
 
 urlWeb = "http://matrix.itasoftware.com/"
 driver = webdriver.Firefox()
@@ -20,8 +23,7 @@ def field_suggestion_box(selector, airport):
     elem = driver.find_element_by_xpath(selector)
     elem.clear()
     elem.send_keys(airport)
-    xpathSelector_ = "//span[contains(text(),'(" + airport + ")')]"
-    el = wait.until(EC.element_to_be_clickable((By.XPATH, xpathSelector_))).click()
+    el = wait.until(EC.element_to_be_clickable((By.XPATH, ("//span[contains(text(),'(" + airport + ")')]")))).click()
 
 
 def field_date_box(selector, data):
@@ -37,7 +39,19 @@ field_suggestion_box(BOX_DEST_, "VLC")
 field_date_box(OUT_DATE_, "08/03/2018")
 field_date_box(RET_DATE_, "08/05/2018")
 
-driver.find_element_by_xpath('//*[@id="searchButton-0"]').click()
+driver.find_element_by_xpath(SEARCH_BUTTON_).click()
 
-h3 = wait.until_not(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[6]/div[5]/div/div/a')))
-print driver.page_source
+h3 = wait.until_not(EC.element_to_be_clickable(
+    (By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[6]/div[5]/div/div/a')))
+
+for ele in driver.find_elements_by_class("IR6M2QD-u-j"):
+    hover = ActionChains(driver).move_to_element(ele)
+    hover.perform()
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+# print(soup.prettify())
+# myDivs = soup.findAll("div", {"class": "IR6M2QD-u-j"})
+#
+# for f in myDivs:
+#     BeautifulSoup(f, 'html.parser').
