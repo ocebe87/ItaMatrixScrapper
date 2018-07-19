7f5d78d38470df98a2995b399d22225d8b70f505
Sergio Ramiro <sergio.ramiro87@gmail.com>
2018-07-20 00:51:36 +0200

    Refactor code

--- sample/pruebaSelenium.py
+++ sample/pruebaSelenium.py
@@ -7,11 +7,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 from sample.Flight import Flight
 
+DIV_ = "div"
+CLASS_ = "class"
+FLIGHT_BOX_ = "IR6M2QD-u-j"
+TIME_DIVS_ = "IR6M2QD-u-f"
+AIRLINE_DIV_ = {CLASS_: "IR6M2QD-u-b"}
 BOX_ORIG_ = '//*[@id="cityPair-orig-0"]'
 BOX_DEST_ = '//*[@id="cityPair-dest-0"]'
 OUT_DATE_ = '//*[@id="cityPair-outDate-0"]'
 RET_DATE_ = '//*[@id="cityPair-retDate-0"]'
 SEARCH_BUTTON_ = '//*[@id="searchButton-0"]'
+LOADING_MESSAGE_ = '/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[6]/div[5]/div/div/a'
 
 urlWeb = "http://matrix.itasoftware.com/"
 driver = webdriver.Firefox()
@@ -19,7 +25,7 @@ driver.get(urlWeb)
 wait = WebDriverWait(driver, 100)
 
 
-def field_suggestion_box(selector, airport):
+def fill_suggestion_box(selector, airport):
     global elem, wait, airportCode, xpathSelector_, el
     elem = driver.find_element_by_xpath(selector)
     elem.clear()
@@ -27,30 +33,42 @@ def field_suggestion_box(selector, airport):
     el = wait.until(EC.element_to_be_clickable((By.XPATH, ("//span[contains(text(),'(" + airport + ")')]")))).click()
 
 
-def field_date_box(selector, data):
+def fill_date_box(selector, data):
     elem = driver.find_element_by_xpath(selector)
     elem.clear()
     elem.send_keys(data)
     elem.send_keys(Keys.INSERT)
 
 
-field_suggestion_box(BOX_ORIG_, "BCN")
-field_suggestion_box(BOX_DEST_, "VLC")
+def search_page():
+    fill_search_fields()
+    driver.find_element_by_xpath(SEARCH_BUTTON_).click()
 
-field_date_box(OUT_DATE_, "08/03/2018")
-field_date_box(RET_DATE_, "08/05/2018")
 
-driver.find_element_by_xpath(SEARCH_BUTTON_).click()
+def fill_search_fields():
+    fill_suggestion_box(BOX_ORIG_, "BCN")
+    fill_suggestion_box(BOX_DEST_, "VLC")
+    fill_date_box(OUT_DATE_, "08/03/2018")
+    fill_date_box(RET_DATE_, "08/05/2018")
 
-h3 = wait.until_not(EC.element_to_be_clickable(
-    (By.XPATH, '/html/body/div[1]/div/div/div/div/div/div[2]/div[1]/div/div[6]/div[5]/div/div/a')))
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-print(soup.prettify())
+def result_page():
+    wait.until_not(EC.element_to_be_clickable(
+        (By.XPATH, LOADING_MESSAGE_)))
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-for f in soup.findAll("div", {"class": "IR6M2QD-u-j"}):
-    airLine = f.find("div", {"class": "IR6M2QD-u-b"}).text
-    airLine = f.findAll("div", {"class": "IR6M2QD-u-f"}).text
-    f.get('a')
+    flights = []
+    for f in soup.findAll(DIV_, {CLASS_: FLIGHT_BOX_}):
+        airLine = f.find(DIV_, AIRLINE_DIV_).string
+        departureGo = f.findAll(DIV_, {CLASS_: TIME_DIVS_})[0].string
+        arriveGo = f.findAll(DIV_, {CLASS_: TIME_DIVS_})[1].string
+        departureCome = f.findAll(DIV_, {CLASS_: TIME_DIVS_})[2].string
+        arriveCome = f.findAll(DIV_, {CLASS_: TIME_DIVS_})[3].string
 
-    flight = Flight(airLine, '', '')
+        flights.append(Flight(airLine, departureGo, arriveGo, departureCome, arriveCome))
+    flights.count()
+
+
+search_page()
+
+result_page()
